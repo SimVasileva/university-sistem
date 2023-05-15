5e2d8c1f24f27b29e096f83452ca33bb254553fc
Sim Vasileva <fixed-term.Simona.Vasileva@bosch.com>
2023-05-15 17:19:18 +0300

    created repositories

---
+++ university-project/src/main/java/com/university/universityproject/repository/StudentRepository.java
@@ -0,0 +1,13 @@
+package com.university.universityproject.repository;
+
+import com.university.universityproject.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    Optional<Student> findByFacultyNumber(String facultyNumber);
+}
